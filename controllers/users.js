f1cae3bc363b4e1cc2deed6510493c45209b364b
Ivan Monichev <ivan@monichev.ru>
2022-09-30 01:12:57 +0300

    feat: change sameSite

--- controllers/users.js
+++ controllers/users.js
@@ -61,7 +61,7 @@ const loginUser = (request, response, next) => {
           response.cookie('access_token', token, {
             maxAge: 1000 * 60 * 60 * 24 * 7,
             httpOnly: true,
-            sameSite: true,
+            sameSite: 'strict',
           });
 
           return response.send({
@@ -75,8 +75,7 @@ const loginUser = (request, response, next) => {
 const logoutUser = (request, response) => {
   response.clearCookie('access_token', {
     httpOnly: true,
-    sameSite: true,
-    secure: true,
+    sameSite: 'strict',
   }).send({
     message: USER_SUCCESS_EXIT,
   });
